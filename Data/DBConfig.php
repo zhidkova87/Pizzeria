a11ad8a6143cae36fe7a89a25566fb4fc6d6807b
Maria <maria.zhidkova@vdabcampus.be>
2022-03-11 12:15:41 +0100

    aangepast

--- Data/DBConfig.php
+++ Data/DBConfig.php
@@ -3,7 +3,19 @@
 namespace Data;
 
 class DBConfig {
-public static $DB_CONNSTRING = "mysql:host=localhost;dbname=pizzeria;charset=utf8";
-public static $DB_USERNAME = "root";
-public static $DB_PASSWORD = "";
+
+    public static $DB_CONNSTRING = "mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_c37b8f27d984651;charset=utf8";
+    public static $DB_USERNAME = "bd1ab42fde31cd";
+    public static $DB_PASSWORD = "ae5f2a29";
+
+//$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+//
+//$server = $url["host"];
+//$username = $url["user"];
+//$password = $url["pass"];
+//$db = substr($url["path"], 1);
+//
+//$conn = new mysqli($server, $username, $password, $db);
+
+//
 }
