c739e3e5f54e08db0c3b49429326d6adf65bafb7
Maria <maria.zhidkova@vdabcampus.be>
2022-03-11 13:52:13 +0100

    aangepast

--- Data/DBConfig.php
+++ Data/DBConfig.php
@@ -4,9 +4,10 @@ namespace Data;
 
 class DBConfig {
 
-    public static $DB_CONNSTRING = "mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_c37b8f27d984651;charset=utf8";
+    public static $DB_CONNSTRING = "mysql: host=us-cdbr-east-05.cleardb.net;dbname=heroku_c37b8f27d984651;charset=utf8";
     public static $DB_USERNAME = "bd1ab42fde31cd";
     public static $DB_PASSWORD = "ae5f2a29";
+
 
 //$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 //
